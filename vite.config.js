680677293a739aec7a9240ede02cf5c6722c6036
Ali ahmed <aa5325368@gmail.com>
2023-04-01 05:31:32 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   
@@ -11,14 +11,11 @@ export default defineConfig({
 
     
     proxy:{
-    "/api": {
-      // target: "http://localhost:8000",
+      "/api": {
       target: "https://smws.onrender.com",
       changeOrigin: true,
       secure: false,
       rewrite:(path)=>path.replace(/^\/api/, ''),
-      ws: true,
-      
     }}
   },
 })
